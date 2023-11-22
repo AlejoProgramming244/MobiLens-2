f0c2e7bfb2dee13b0eb14d8f32875718bb3f5f8a
AlejoProgramming244 <142456180+AlejoProgramming244@users.noreply.github.com>
2023-11-22 16:50:25 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function Isrc()
+{
+    input = document.getElementById("input").value;
+
+    document.getElementById("image").innerHTML = "<img src="+ input +" width='300px' height='423px' id='image1'>"
+}
